2fdfb815826e05cdda90dd93b63ada9534acc745
talentedexpert0057 <petpanda0057@gmail.com>
2021-01-29 07:43:21 +0800

    added welcome page of auth

--- resources/views/auth/welcome.blade.php
+++ resources/views/auth/welcome.blade.php
@@ -5,111 +5,23 @@
     <div class="w-100 p-3 px-md-5">
         <div class="d-flex py-5 head">
             <div class="m-auto text-center container-size-md">
-                <h2 class="title">Mi agenda</h2>
-                <p>Ve todos los eventos que tendremos a lo largo de la Expo y <a href="" class="yellow-link hover-dark">selecciona los que sean de tu interés</a>, para recibir recordatorios a tu email y celular.</p>
+                <h2 class="title">¡Bienvenidos!</h2>
+                <p>
+                    Estamos muy emocionados de tenerlos con nosotros. <br>
+                    Queremos mostrarles todo lo que tenemos preparado para ustedes.
+                </p>
             </div>
         </div>
-        <div class="body">
-            <div class="comming-event-list  wow animated fadeInLeft">
-                <div class="scrollbox" style="max-height: 433px;">
-                    {{-- comming event item --}}
-                    <div class="comming-event-item">
-                        <div class="date d-flex">
-                            <div class="m-auto">
-                            <span class="day">18 Nov</span>
-                            <span class="time">10:00 AM</span>
-                            </div>
-                        </div>
-                        <div class="comming-event-body">
-                            <div class="head">
-                            <span class="title">Lorem ipsum dolor sit amet...</span>
-                            <span class="star">
-                                <i class="fa fa-star-o"></i>
-                                Me interesa
-                            </span>
-                            </div>
-                            <span class="brief">
-                            Lorem ipsum dolor sit amet, c sed diam nonummy nibh euismod 
-                            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi 
-                            enim ad minim veniam.
-                            </span>
-                        </div>
-                    </div>
-                    {{-- end comming event item --}}
-                    {{-- comming event item --}}
-                    <div class="comming-event-item">
-                        <div class="date d-flex">
-                            <div class="m-auto">
-                            <span class="day">18 Nov</span>
-                            <span class="time">10:00 AM</span>
-                            </div>
-                        </div>
-                        <div class="comming-event-body">
-                            <div class="head">
-                            <span class="title">Lorem ipsum dolor sit amet...</span>
-                            <span class="star star-yellow">
-                                <i class="fa fa-star"></i>
-                                Me interesa
-                            </span>
-                            </div>
-                            <span class="brief">
-                            Lorem ipsum dolor sit amet, c sed diam nonummy nibh euismod 
-                            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi 
-                            enim ad minim veniam.
-                            </span>
-                        </div>
-                    </div>
-                    {{-- end comming event item --}}
-                    {{-- comming event item --}}
-                    <div class="comming-event-item">
-                        <div class="date d-flex">
-                            <div class="m-auto">
-                            <span class="day">18 Nov</span>
-                            <span class="time">10:00 AM</span>
-                            </div>
-                        </div>
-                        <div class="comming-event-body">
-                            <div class="head">
-                            <span class="title">Lorem ipsum dolor sit amet...</span>
-                            <span class="star star-yellow">
-                                <i class="fa fa-star"></i>
-                                Me interesa
-                            </span>
-                            </div>
-                            <span class="brief">
-                            Lorem ipsum dolor sit amet, c sed diam nonummy nibh euismod 
-                            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi 
-                            enim ad minim veniam.
-                            </span>
-                        </div>
-                    </div>
-                    {{-- end comming event item --}}
-                    {{-- comming event item --}}
-                    <div class="comming-event-item">
-                        <div class="date d-flex">
-                            <div class="m-auto">
-                            <span class="day">18 Nov</span>
-                            <span class="time">10:00 AM</span>
-                            </div>
-                        </div>
-                        <div class="comming-event-body">
-                            <div class="head">
-                            <span class="title">Lorem ipsum dolor sit amet...</span>
-                            <span class="star star-yellow">
-                                <i class="fa fa-star"></i>
-                                Me interesa
-                            </span>
-                            </div>
-                            <span class="brief">
-                            Lorem ipsum dolor sit amet, c sed diam nonummy nibh euismod 
-                            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi 
-                            enim ad minim veniam.
-                            </span>
-                        </div>
-                    </div>
-                    {{-- end comming event item --}}
-                </div>
-            </div>   
+        <div class="d-flex body">
+            <div class="m-auto d-flex container-size-lg">
+                <div class="video-bg d-flex">   
+                    <a href="" class="m-auto btn-video-circle">
+                        <div class="bg-btn-circle">
+                            <span class="btn-circle-text"><i class="fa fa-caret-right"></i></span>                        
+                        </div>       
+                    </a>              
+                </div>           
+            </div>             
         </div>
         <div class="footer">
             <div class="pt-4 text-end">
