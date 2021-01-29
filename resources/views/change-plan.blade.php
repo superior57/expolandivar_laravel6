2f88d393155d1179d5bfac9268435d6d370321ba
talentedexpert0057 <petpanda0057@gmail.com>
2021-01-29 17:47:46 +0800

    added change plan page

--- resources/views/change-plan.blade.php
+++ resources/views/change-plan.blade.php
@@ -10,9 +10,30 @@
 
 @section('content')
 <section class="change-plan py-5">
-  <h2 class="title text-center mb-4">Cambiar campus o plan de estudio</h2>
-  <div class="w-100">
-    
+  <h2 class="title text-center mb-5">Cambiar campus o plan de estudio</h2>
+  <div class="w-100 container-size-sm m-auto">
+    <div class="w-100 form-group mb-2">
+        <label for="base_location">Campus o sede</label> <br>
+        <span>
+          También podrás visitar otros campus o sedes luego
+        </span>
+        <select class="form-control" name="base_location" id="base_location" required>
+            <option value="1">Please select</option>
+            <option value="2">Ciudad de Guatemala</option>
+            <option value="3">Ciudad de Guatemala</option>
+        </select>
+    </div>
+    <div class="w-100 form-group mb-5">
+        <label for="base_location">Ciudad de Guatemala</label> <br>
+        <select class="form-control" name="base_location" id="base_location" required>
+            <option value="1">Please select</option>
+            <option value="2">Pregrado</option>
+            <option value="3">Pregrado</option>
+        </select>
+    </div>
+    <div class="w-100 text-center">
+      <a href="" class="btn btn-primary">Guardar</a>
+    </div>
   </div>  
 </section>
 @endsection
